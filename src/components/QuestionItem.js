186c28cba8b984fe3e670c8bfa3a6d90a880743a
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-05-08 22:33:57 +0300

    final commit

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,14 +1,23 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteItem}) {
   const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
+  const options =answers && answers.length>0 ? answers.map((answer, index) => (
     <option key={index} value={index}>
+      {''} console.log(question);
       {answer}
     </option>
-  ));
+  ))
+  : null
 
+  const handleDelete=()=>{
+    console.log(question.id);
+    fetch(`http://localhost:4000/questions/${question.id}`,{
+      method:'DELETE',
+    })
+    .then(()=>onDeleteItem(question.id))
+  };
   return (
     <li>
       <h4>Question {id}</h4>
@@ -17,7 +26,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+  const[question,setQuestion]=useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:4000/questions')
+    .then((res)=>res.json())
+    .then((question)=>setQuestion(question))
+  },[])
+
+  function handleDeletedQuestion(deletedItem){
+     console.log("done",deletedItem);
+     const updateItems = question && question.filter((quest) =>  
+      quest.id !== deletedItem);
+      setQuestion(updateItems);
+  }
   return (
     <section>
-      <h1>Quiz Questions</h1>
+      <h1>Quiz Questions{question && question.map((question)=>(
+       <QuestionItem
+       key={question.id}
+       question={question}
+       onDeleteItem={handleDeletedQuestion}
+        /> 
+      ))}
+      </h1>
       <ul>{/* display QuestionItem components here after fetching */}</ul>
     </section>
   );
